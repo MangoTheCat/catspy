1ebfb752de3128ada0067251720dcc9b24e61a9d
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2019-02-28 12:49:31 +0000

    added documentation

--- forcats/cat_functions/cat_lump.py
+++ forcats/cat_functions/cat_lump.py
@@ -1,4 +1,21 @@
 def cat_lump(x, n = 5, other_level = "Other"):
+    '''
+    Lump together least common categories into an "Other" category
+    
+    Parameters
+    ----------
+    x : pd.Series
+      series to be modified
+    n : int
+      number of levels to preserve
+    other_level : str
+      "other" category label
+      
+    Returns
+    -------
+    y : pd.Series
+      modified series (with categorical type)
+    '''
     counts = x.value_counts()
     if (len(counts) > n):
         repl = counts.iloc[n:].index
