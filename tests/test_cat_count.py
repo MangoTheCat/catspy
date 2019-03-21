63cceedd79f8f3b9f038502db94356324c701d4a
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-03-21 23:04:33 +0000

    updates to tests

--- tests/test_cat_count.py
+++ tests/test_cat_count.py
@@ -18,22 +18,34 @@ def data():
 
 def test_cat_count_on_string_column(data):
 
-    result = cat_count(data.A)
+   result = cat_count(data.A, sort = False)
+
+   target = pd.DataFrame({
+       'f': {0: 'a', 1: 'b', 2: 'c', 3: 'd'},
+       'n': {0: 4, 1: 3, 2: 2, 3: 1},
+       })
+   assert result.f.tolist() == target.f.tolist()
+
+def test_cat_count_on_string_column(data):
+
+    result = cat_count(data.A, sort = True)
 
     target = pd.DataFrame({
         'f': {0: 'a', 1: 'b', 2: 'c', 3: 'd'},
         'n': {0: 4, 1: 3, 2: 2, 3: 1},
         })
-    assert result == target
-
+    assert result.f.tolist() == target.f.tolist()
+    assert result.n.tolist() == target.n.tolist()
 
 def test_cat_count_on_categorical_column(data):
 
-    result = cat_count(df.B, prop = True)
+    result = cat_count(data.B, prop = True)
 
     target = pd.DataFrame({
         'f': {0: 'a', 1: 'b', 2: 'c', 3: 'd'},
         'n': {0: 4, 1: 3, 2: 2, 3: 1},
         'p': {0: 0.4, 1: 0.3, 2: 0.2, 3: 0.1}
         })
-    assert result == target
+    assert result.f.tolist() == target.f.tolist()
+    assert result.n.tolist() == target.n.tolist()
+    assert result.p.tolist() == target.p.tolist()
